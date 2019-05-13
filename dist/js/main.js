f30530635a2bb87b14d12888b83c6069f7c31911
Wok Seng <wok@ualberta.ca>
2019-05-13 11:40:58 +0800

    Modify menu layout

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const menuTesting = document.querySelector(".menu-testing");
 
 //Set Initial State Of Menu
 let showMenu = false;
@@ -17,6 +18,7 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
+    menuTesting.classList.add("show");
 
     // Set Menu State
     showMenu = true;
@@ -26,6 +28,7 @@ function toggleMenu() {
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
+    menuTesting.classList.remove("show");
 
     // Set Menu State
     showMenu = false;
