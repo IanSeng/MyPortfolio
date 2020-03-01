1f515844d853a05a9d2e23acb1dbc4a35a844dac
Ian Seng <ianseng@outlook.com>
2020-03-01 17:18:37 -0500

    able to get repo languages

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,32 @@ async function getUserRepos() {
     const response = await fetch(url)
     const result = await response.json()
 
-    console.log(result)
+    //console.log(result)
+
+    // Object.keys(result).forEach(key => {
+    //     console.log(result[key].name)
+    //   });
 
     Object.keys(result).forEach(key => {
-        console.log(result[key].name)
-      });
+        getRepoLanguages(result[key].name);
+    });
 
 
 }
 
+async function getRepoLanguages(repoName: string) {
+    const url = `https://api.github.com/repos/${GITHUB_USERNAME}/${repoName}/languages`
+    const response = await fetch(url)
+    const result = await response.json()
+    console.log(repoName)
+
+    console.log(result)
+
+    
+
+
+}
+
+//getUserRepos();
 getUserRepos();
 
