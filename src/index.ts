aaa9f0cdea8ea563faa9d54ed4f0a06ba31043f4
Ian Seng <ianseng@outlook.com>
2020-03-15 00:22:44 -0400

    create repo object with name url languages

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { default as data } from "./data.json";
 async function hello() {
     //let repos: Repo[] = [] 
 
-    // const userRepoData = await GithubServices.getUserRepos();
+    const userRepoData = await GithubServices.getUserRepos();
+    let chain: any = [];
     // console.log(JSON.stringify(userRepoData))
 
     // userRepoData.forEach(async element => {
@@ -21,28 +22,38 @@ async function hello() {
     //repos.push(new Repo('hewwrh',"ee",["ee"]))
     //repos.push(new Repo('wok-FeelsBook',"https://github.com/IanSeng/wok-FeelsBoo",["java"]))
     //console.log(repos)
-    let repoNameList: any[] = []
-    let repoLanguage: any[] = []
-    data.forEach(element => {
-        repoNameList.push(element.name)
-    })
+    // let repoNameList: any[] = []
+    // let repoLanguage: any[] = []
+    // data.forEach(element => {
+    //     repoNameList.push(element.name)
+    // })
 
-    await Promise.all(repoNameList.map(async (element) => {
-        const contents = await GithubServices.getRepoLanguages(element);
-        console.log(element)
-        console.log(contents)
-      }));
+    await Promise.all(userRepoData.map(async (element: any) => {
+        console.log(element.name)
+        const getRepoLanguages: any[] = await GithubServices.getRepoLanguages(element.name);
+        
+        chain.push(new Repo(element.name, element.html_url, getRepoLanguages))
+
+        //console.log(element)
+        //console.log(contents)
+    }));
+    console.log(chain)
+    // let x = new Repo("hu", "this", ['he', 'le'])
+    // chain.push(x)
+    // let y = new Repo("hqexx", "this", ['he', 'le'])
+    // chain.push(y)
+    
     // console.log(repoNameList)
     // repoNameList.forEach(async element => {
     //     let y = await GithubServices.getRepoLanguages(element);
     //     //repoLanguage.push([element,y])
     //     console.log(element)
     // })
-    
+
     //let y = await GithubServices.getRepoLanguages("blockchain");
-        //console.log(await GithubServices.getRepoLanguages(element.name))
-        //console.log(y)
-        
+    //console.log(await GithubServices.getRepoLanguages(element.name))
+    //console.log(y)
+
 
 
 
