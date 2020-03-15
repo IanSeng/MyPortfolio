46adbeb97ba841180b17596d68a94dbbb6ca9d97
Ian Seng <ianseng@outlook.com>
2020-03-15 01:03:18 -0400

    produce json file with list of repo

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,32 @@
 import { GithubServices } from "./services/github-services"
 import { Repo } from "./repo";
-import { default as data } from "./data.json";
+
+const fs = require('fs');
 
 async function hello() {
     //let repos: Repo[] = [] 
 
     const userRepoData = await GithubServices.getUserRepos();
-    let chain: any = [];
-    // console.log(JSON.stringify(userRepoData))
+    let repoList: any = [];
 
-    // userRepoData.forEach(async element => {
-    //     let y = await GithubServices.getRepoLanguages(element.name);
-    //     //console.log(await GithubServices.getRepoLanguages(element.name))
-    //     //console.log(y)
-    //     repos.push(new Repo("hi", "element.htmlUrl", ["hi"]))
-    //     console.log(repos)
-    // })
-    // let x = new Repo('heh',"ee",["ee"])
 
-    //repos.push(x)
-    //repos.push(new Repo('hewwrh',"ee",["ee"]))
-    //repos.push(new Repo('wok-FeelsBook',"https://github.com/IanSeng/wok-FeelsBoo",["java"]))
-    //console.log(repos)
-    // let repoNameList: any[] = []
-    // let repoLanguage: any[] = []
-    // data.forEach(element => {
-    //     repoNameList.push(element.name)
-    // })
+    await Promise.all(userRepoData.map(async (repo: any) => {
+        //console.log(repo.name)
+        const getRepoLanguages: any[] = await GithubServices.getRepoLanguages(repo.name);
+        repoList.push(new Repo(repo.name, repo.html_url, getRepoLanguages))
 
-    await Promise.all(userRepoData.map(async (element: any) => {
-        console.log(element.name)
-        const getRepoLanguages: any[] = await GithubServices.getRepoLanguages(element.name);
-        
-        chain.push(new Repo(element.name, element.html_url, getRepoLanguages))
 
-        //console.log(element)
-        //console.log(contents)
+
+
     }));
-    console.log(chain)
-    // let x = new Repo("hu", "this", ['he', 'le'])
-    // chain.push(x)
-    // let y = new Repo("hqexx", "this", ['he', 'le'])
-    // chain.push(y)
-    
-    // console.log(repoNameList)
-    // repoNameList.forEach(async element => {
-    //     let y = await GithubServices.getRepoLanguages(element);
-    //     //repoLanguage.push([element,y])
-    //     console.log(element)
-    // })
-
-    //let y = await GithubServices.getRepoLanguages("blockchain");
-    //console.log(await GithubServices.getRepoLanguages(element.name))
-    //console.log(y)
-
+   
+    fs.writeFile("./repoList.json", JSON.stringify(repoList), 'utf8', function (err: any) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    });
 
 
 
