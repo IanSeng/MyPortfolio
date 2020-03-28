4bf1ec0a6bd30e000afc7331f64fbec42e550d3c
Ian Seng <ianseng@outlook.com>
2020-03-27 20:17:46 -0400

    even from current date

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,25 @@ const fs = require('fs');
 async function hello() {
     //let repos: Repo[] = [] 
 
-    const userRepoData = await GithubServices.getUserRepos();
-    let repoList: any = [];
+    // const userRepoData = await GithubServices.getUserRepos();
+    // let repoList: any = [];
 
 
-    await Promise.all(userRepoData.map(async (repo: any) => {
-        //console.log(repo.name)
-        const getRepoLanguages: any[] = await GithubServices.getRepoLanguages(repo.name);
-        repoList.push(new Repo(repo.name, repo.html_url, getRepoLanguages))
-
-
-
-
-    }));
+    // await Promise.all(userRepoData.map(async (repo: any) => {
+    //     //console.log(repo.name)
+    //     const getRepoLanguages: any[] = await GithubServices.getRepoLanguages(repo.name);
+    //     repoList.push(new Repo(repo.name, repo.html_url, getRepoLanguages))
+    // }));
    
-    fs.writeFile("./repoList.json", JSON.stringify(repoList), 'utf8', function (err: any) {
-        if (err) {
-            console.log("An error occured while writing JSON Object to File.");
-            return console.log(err);
-        }
-        console.log("JSON file has been saved.");
-    });
+    // fs.writeFile("./repoList.json", JSON.stringify(repoList), 'utf8', function (err: any) {
+    //     if (err) {
+    //         console.log("An error occured while writing JSON Object to File.");
+    //         return console.log(err);
+    //     }
+    //     console.log("JSON file has been saved.");
+    // });
 
-
-
+    GithubServices.loadClient();
 
 }
 
--- src/services/github-services.ts
+++ src/services/github-services.ts
@@ -1,39 +1,69 @@
 import GITHUB_USERNAME from "../app/config/config";
 import { Repo } from "../repo";
 const fetch = require("node-fetch");
+const { google } = require('googleapis');
+const calendar = google.calendar({ version: 'v3', auth: "AIzaSyCJodvMWwAO6GUmYimERL3rJOu40GlZAJM" });
 
 module GithubServices {
-    let userRepoList: Array<any> = [];
-    let repos: Repo[] = []
+  let userRepoList: Array<any> = [];
+  let repos: Repo[] = []
 
 
-    export async function getUserRepos() {
-        const url = `https://api.github.com/users/${GITHUB_USERNAME}/repos`
-        const response = await fetch(url)
-        const result = await response.json()
+  export async function getUserRepos() {
+    const url = `https://api.github.com/users/${GITHUB_USERNAME}/repos`
+    const response = await fetch(url)
+    const result = await response.json()
 
-        // Object.keys(result).forEach(async key => {
-        //     const repoLan: any = (await getRepoLanguages(result[key].name))
-        //     repos.push(result[key].name, result[key].html_url, repoLan)
+    // Object.keys(result).forEach(async key => {
+    //     const repoLan: any = (await getRepoLanguages(result[key].name))
+    //     repos.push(result[key].name, result[key].html_url, repoLan)
 
-        // });
+    // });
 
-        return result
+    return result
 
-    }
+  }
 
-    export async function getRepoLanguages(repoName: string) {
-        const url = `https://api.github.com/repos/${GITHUB_USERNAME}/${repoName}/languages`
-        const response = await fetch(url)
-        const result = await response.json()
-        let repoLanguages: Array<any> = [];
-        //console.log(result)
+  export async function getRepoLanguages(repoName: string) {
+    const url = `https://api.github.com/repos/${GITHUB_USERNAME}/${repoName}/languages`
+    const response = await fetch(url)
+    const result = await response.json()
+    let repoLanguages: Array<any> = [];
+    //console.log(result)
 
-        Object.keys(result).forEach(key => {
-            repoLanguages.push(key)
+    Object.keys(result).forEach(key => {
+      repoLanguages.push(key)
+    });
+    return repoLanguages;
+  }
+
+
+  export async function loadClient() {
+
+    calendar.events.list({
+      calendarId: 'ian.dev27@gmail.com',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+
+    }, (err: any, res: any) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const events = res.data.items;
+      if (events.length) {
+        console.log('Upcoming 10 events:');
+        events.map((event: any, i: any) => {
+          
+          console.log(JSON.stringify(event))
+          //const start = event.start.dateTime || event.start.date;
+          //console.log(`${start} - ${event.summary}`);
         });
-        return repoLanguages;
-    }
+      } else {
+        console.log('No upcoming events found.');
+      }
+    });
+
+  }
 }
 
 export { GithubServices };
